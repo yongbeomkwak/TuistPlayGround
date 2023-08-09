1ee9f578ea939f54f55f96ea3fc58c64ca04868b
Hamp <yongbeomkwak@gmail.com>
2023-08-09 23:44:09 +0900

    :tada:: Config + XCconfig

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -4,17 +4,23 @@
 //Dependencies의 SPM에 Package를 추가(Local 또는 Remote) 하고 dependencies에서 .external로 등록합니다.
 
 import ProjectDescription
-
+import HampPlugin
 
 let dependencies = Dependencies(
     carthage: [],
     swiftPackageManager: SwiftPackageManagerDependencies([
-        .remote(url: "https://github.com/ReactiveX/RxSwift.git",requirement: .upToNextMajor(from: "6.5.0"))
+        .remote(url: "https://github.com/ReactiveX/RxSwift.git",requirement: .upToNextMajor(from: "6.5.0")),
+        .remote(url: "https://github.com/airbnb/lottie-ios.git", requirement: .upToNextMajor(from: "3.5.0")),
+        .remote(url: "https://github.com/uber/needle.git", requirement: .upToNextMajor(from: "0.19.0")),
+        .remote(url: "https://github.com/Moya/Moya.git", requirement: .upToNextMajor(from: "15.0.3")),
+        .remote(url: "https://github.com/Quick/Nimble.git", requirement: .upToNextMajor(from: "10.0.0")),
+        .remote(url: "https://github.com/Quick/Quick.git", requirement: .upToNextMajor(from: "5.0.0"))
     ],
     baseSettings: .settings(
         configurations: [
-            .debug(name: .debug),
-            .release(name: .release)
+            .debug(name: .dev),
+            .debug(name: .stage),
+            .release(name: .prod)
         ]
     )),
     
